8cd2f9880ad73a01410705de9d3758452575f471
Kstuersel <kstuersel@carthage.edu>
2025-03-06 13:23:32 -0600

    added square function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+void square(int numToBeSquared);
 
 int main()
 {
 	cout << "GitHub Test" << endl;
+	square(7);
+}
+
+void square(int squareNum)
+{
+	cout << (squareNum * squareNum) << endl;
 }
